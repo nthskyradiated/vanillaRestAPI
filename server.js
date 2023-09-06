890758ebbeb1781f07fa274753883ea1282dec06
nthskyradiated <andy.pandaan@outlook.com>
2023-09-06 11:23:02 +0400

    updated RegEx

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const http = require('http')
-const { getProducts, getProduct,createProduct, updateProduct, deleteProduct } = require('./controllers/productController')
+const {
+     getProducts, 
+     getProduct,
+     createProduct, 
+     updateProduct, 
+     deleteProduct 
+    } = require('./controllers/productController')
 
 const PORT = process.env.PORT || 5000
 const server = http.createServer((req, res) => {
 
     if (req.url === '/api/products' && req.method === 'GET') {
         getProducts(req, res)
-    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
+    } else if (req.url.match(/\/api\/products\/\w+/) && req.method === 'GET') {
         const id = req.url.split('/')[3]
         getProduct(req, res, id)
     } else if (req.url === '/api/products' && req.method === 'POST') {
         createProduct(req, res)
-    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
+    } else if (req.url.match(/\/api\/products\/\w+/) && req.method === 'PUT') {
         const id = req.url.split('/')[3]
         updateProduct(req, res, id)
-    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE') {
+    } else if (req.url.match(/\/api\/products\/\w+/) && req.method === 'DELETE') {
         const id = req.url.split('/')[3]
         deleteProduct(req, res, id)
     }else {
